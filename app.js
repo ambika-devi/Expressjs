5b5e337e38bc8b615d9a478f268e1a12241b65cb
ambika-devi <ambikadevithota5@gmail.com>
2022-11-18 19:12:29 +0530

    routes/filters

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
+const adminRoutes=require('./routes/admin.js');
+const shopRoutes=require('./routes/shop.js');
+const { route } = require('./routes/admin.js');
 app.use(bodyParser.urlencoded({extended:false}))
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="message"><button type="submit">ADD PRODUCT</button></form>');
 
-})
-app.use('/product',(req,res,next)=> {
-    console.log(req.body);//printing the request from client
-    res.redirect('/');//redirecting to current route
-})
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello From Express.js</h1>');
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 })
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 //Backened
 const express=require('express');
 const router=express.Router();
+//admin/add-product=>GET
 router.get('/add-product',(req,res,next)=>{
     res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="message"><button type="submit">ADD PRODUCT</button></form>');
 
 })
+//admin/add-product=>POST
 router.post('/add-product',(req,res,next)=> {
     console.log(req.body);//printing the request from client
     res.redirect('/');//redirecting to current route
